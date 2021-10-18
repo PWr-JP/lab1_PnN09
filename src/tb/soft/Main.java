95c92f680882c4a42a15d2bdb4f337e0fc11f691
Tomasz Babczyński <tomasz.babczynski@pwr.edu.pl>
2021-10-18 10:53:29 +0200

    dodanie zmiennych

--- src/tb/soft/Main.java
+++ src/tb/soft/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj");
+        String witaj,imie;
+
+        witaj="Witaj";
+        imie=args[0];
+
+        System.out.printf("%s posiadaczu imienia: %s\n%n",witaj, imie);
 
         System.exit(0);
     }
